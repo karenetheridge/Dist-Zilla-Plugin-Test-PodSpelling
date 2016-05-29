efd3b2901411d5245c3ae00e585fb51f8c49e075
Karen Etheridge <ether@cpan.org>
2016-05-28 21:10:03 -0700

    increment $VERSION after 2.007002 release

--- lib/Dist/Zilla/Plugin/Test/PodSpelling.pm
+++ lib/Dist/Zilla/Plugin/Test/PodSpelling.pm
@@ -5,7 +5,7 @@ package Dist::Zilla::Plugin::Test::PodSpelling;
 # ABSTRACT: Author tests for POD spelling
 # KEYWORDS: plugin test spelling words stopwords typos errors documentation
 
-our $VERSION = '2.007002';
+our $VERSION = '2.007003';
 
 use Moose;
 extends 'Dist::Zilla::Plugin::InlineFiles';
